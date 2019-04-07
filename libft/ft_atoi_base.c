9618affee73957e9dd66b381600c371f2b8f1d46
lreznak- <andreybrazhenko@yandex.ru>
2019-04-07 09:29:30 +0300

    libft norm

--- libft/ft_atoi_base.c
+++ libft/ft_atoi_base.c
@@ -6,69 +6,70 @@
 /*   By: wclayton <wclayton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 21:25:23 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/04/03 18:45:14 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/04/07 09:28:51 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static int		ft_power(int nb, int power)
+
+static int			ft_power(int nb, int power)
 {
-		if (power == 0)
-					return (1);
-			else if (power < 0)
-						return (0);
-				return (nb * ft_power(nb, power - 1));
+	if (power == 0)
+		return (1);
+	else if (power < 0)
+		return (0);
+	return (nb * ft_power(nb, power - 1));
 }
 
-static int	convert_and_check_nb(char c, int base)
+static int			convert_and_check_nb(char c, int base)
 {
-		int		result;
+	int		result;
 
-			if (c >= '0' && c <= '9')
-						result = c - 48;
-				else if (c >= 'a' && c <= 'z')
-							result = c - 97 + 10;
-					else if (c >= 'A' && c <= 'Z')
-								result = c - 65 + 10;
-						else
-									result = -1;
-							if (result < base && result != -1)
-										return (result);
-								else
-											return (-1);
+	if (c >= '0' && c <= '9')
+		result = c - 48;
+	else if (c >= 'a' && c <= 'z')
+		result = c - 97 + 10;
+	else if (c >= 'A' && c <= 'Z')
+		result = c - 65 + 10;
+	else
+		result = -1;
+	if (result < base && result != -1)
+		return (result);
+	else
+		return (-1);
 }
 
-static int	length_number(char *str, int base)
+static int			length_number(char *str, int base)
 {
-		int		length;
+	int		length;
 
-			length = 0;
-				while (str[length])
-						{
-									if (convert_and_check_nb(str[length], base) == -1)
-													break ;
-											length++;
-												}
-					return (length);
+	length = 0;
+	while (str[length])
+	{
+		if (convert_and_check_nb(str[length], base) == -1)
+			break ;
+		length++;
+	}
+	return (length);
 }
 
-int			ft_atoi_base(char *nb, int base)
+int					ft_atoi_base(char *nb, int base)
 {
-		int		result;
-			long long	length;
+	int			result;
+	long long	length;
 
-				if (base == 10)
-							return (ft_atoi(nb));
-					while (*nb == ' ' || *nb == '\t' || *nb == '\n'
-										|| *nb == '\v' || *nb == '\r' || *nb == '\f')
-								nb++;
-			result = 0;
-			length = length_number(nb, base) - 1;
-			while (*nb && length >= 0 && convert_and_check_nb(*nb, base) != -1)
-			{
-				result += convert_and_check_nb(*nb, base) * ft_power(base, length);
-				nb++;
-				length--;
-			}
-									return ((int)result);
+	if (base == 10)
+		return (ft_atoi(nb));
+	while (*nb == ' ' || *nb == '\t' || *nb == '\n'
+		|| *nb == '\v' || *nb == '\r' || *nb == '\f')
+		nb++;
+	result = 0;
+	length = length_number(nb, base) - 1;
+	while (*nb && length >= 0 && convert_and_check_nb(*nb, base) != -1)
+	{
+		result += convert_and_check_nb(*nb, base) * ft_power(base, length);
+		nb++;
+		length--;
+	}
+	return ((int)result);
 }
