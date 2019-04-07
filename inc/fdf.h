17871d814a8eb73e9df0b58cf7c50ecf180560f6
William mooton Clayton suggs <wclayton@at-i5.21-school.ru>
2019-04-07 10:31:18 +0300

    norme incs ok

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,14 +6,19 @@
 /*   By: wclayton <wclayton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 21:26:36 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/04/07 10:03:19 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/04/07 10:30:27 by wclayton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-
-/* PASS */
+# define DIR_ERROR			1
+# define ERROR_BIG_FILE		3
+# define FILE_ERROR			2
+# define HEIGHT 1200
+# define MINIMUM_RADIAN		0.2
+# define CL (tgetstr("cl", NULL))
+# define WIDTH 1600
 # include <sys/stat.h>
 # include <stdlib.h>
 # include <fcntl.h>
@@ -25,68 +30,57 @@
 # include "libft.h"
 # include <sys/ioctl.h>
 
-# define DIR_ERROR			1
-# define FILE_ERROR			2
-# define ERROR_BIG_FILE		3
-
-# define WIDTH 1600
-# define HEIGHT 1200
-
-# define MINIMUM_RADIAN		0.2
-
-# define CL (tgetstr("cl", NULL))
-
-typedef struct	s_dot
+typedef struct		s_dot
 {
-	double 	x;
-	double 	y;
-	double 	z;
-	int color;
-	unsigned char r;
-	unsigned char g;
-	unsigned  char b;
-}				t_dot;
+	double			x;
+	double			y;
+	double			z;
+	int				color;
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+}					t_dot;
 
-typedef struct	s_pot
+typedef struct		s_pot
 {
-	double 	r;
-	double 	g;
-	double 	b;
-}				t_pot;
+	double			r;
+	double			g;
+	double			b;
+}					t_pot;
 
-typedef struct	s_pxl
+typedef struct		s_pxl
 {
-	double	dx;
-	double	dy;
-	double	grad;
-	double	xend;
-	double	yend;
-	double	xgap;
-	double	xpxl1;
-	double	ypxl1;
-	double	xpxl2;
-	double	ypxl2;
-	int		steep;
-	int		i;
-	double	intery;
-	int		flag;
-	t_pot	plc;
-}				t_pxl;
+	double			dx;
+	double			dy;
+	double			grad;
+	double			xend;
+	double			yend;
+	double			xgap;
+	double			xpxl1;
+	double			ypxl1;
+	double			xpxl2;
+	double			ypxl2;
+	int				steep;
+	int				i;
+	double			intery;
+	int				flag;
+	t_pot			plc;
+}					t_pxl;
 
-typedef struct 	s_dots
+typedef struct		s_dots
 {
-	t_dot	angle;
-	int		cols;
-	int		rows;
-	int		maxh;
-	int		minh;
-	t_dot	anc;
-	t_dot	**dots;
-	t_dot	ani;
-	t_dot	anx;
-	t_dot	any;
-	t_dot	anz;
-}				t_dots;
+	t_dot			angle;
+	int				cols;
+	int				rows;
+	int				maxh;
+	int				minh;
+	t_dot			anc;
+	t_dot			**dots;
+	t_dot			ani;
+	t_dot			anx;
+	t_dot			any;
+	t_dot			anz;
+}					t_dots;
 
 typedef struct		s_mouse
 {
@@ -106,56 +100,49 @@ typedef struct		s_keyboard
 	int				pressed[256];
 }					t_keyboard;
 
-
 typedef struct		s_fdf
 {
 	t_mouse			*mouse;
 	t_keyboard		*keyboard;
 	t_dots			*map;
 	void			*mlx_ptr;
-    void			*win_ptr;
+	void			*win_ptr;
 	char			*img_data;
 	void			*img_ptr;
 	int				bpp;
 	int				sl;
 	int				endian;
-	int				projection;
 	int				is_chill;
 }					t_fdf;
 
-t_dots			*map_parser(const char *path);
-int 			file_exit(int code);
-void			wu(char *data, t_dot p1, t_dot p2);
-int				pretty_terminal(int status);
-void			map_twister_x(t_dots *map, int mode, double angle);
-void			map_twister_y(t_dots *map, int mode, double angle);
-void			map_twister_z(t_dots *map, int mode, double angle);
-void			move_basis(t_dots *map);
-void			remove_basis(t_dots *map);
-t_dot			anchor(t_dots *map);
-void			map_scale(t_dots *map, int mode);
-void			tty_print(char *type, float data, char *str);
-void			plot(t_xy xy, char *data, double bs, t_pot rgb);
-void			unplot(char *data);
-void			swap(double *a, double *b);
-void			iswap(unsigned char *a, unsigned char *b);
-double			ipart(double x);
-double			round(double x);
-double			fpart(double x);
-double			rfpart(double x);
-int		drawmap(t_fdf *fdf);
-int		key_press(int keycode, t_fdf *fdf);
-int		key_release(int keycode, t_fdf *fdf);
-int		mouse_press(int button, int x, int y, t_fdf *fdf);
-int		close1(t_fdf *fdf);
-
-
-
-
-int				fdf_exit(void *data);
-int				file_exit(int code);
-
-int				tupa_chill(t_fdf *fdf);
-
+t_dots				*map_parser(const char *path);
+int					file_exit(int code);
+void				wu(char *data, t_dot p1, t_dot p2);
+int					pretty_terminal(int status);
+void				map_twister_x(t_dots *map, int mode, double angle);
+void				map_twister_y(t_dots *map, int mode, double angle);
+void				map_twister_z(t_dots *map, int mode, double angle);
+void				move_basis(t_dots *map);
+void				remove_basis(t_dots *map);
+t_dot				anchor(t_dots *map);
+void				map_scale(t_dots *map, int mode);
+void				tty_print(char *type, float data, char *str);
+void				plot(t_xy xy, char *data, double bs, t_pot rgb);
+void				unplot(char *data);
+void				swap(double *a, double *b);
+void				iswap(unsigned char *a, unsigned char *b);
+double				ipart(double x);
+double				round(double x);
+double				fpart(double x);
+double				rfpart(double x);
+int					drawmap(t_fdf *fdf);
+int					key_press(int keycode, t_fdf *fdf);
+int					key_release(int keycode, t_fdf *fdf);
+int					mouse_press(int button, int x, int y, t_fdf *fdf);
+int					close1(t_fdf *fdf);
+void				map_chill_twister(t_dots *map, int mode);
+int					fdf_exit(void *data);
+int					file_exit(int code);
+int					tupa_chill(t_fdf *fdf);
 
 #endif
--- inc/libft.h
+++ inc/libft.h
@@ -6,7 +6,7 @@
 /*   By: wclayton <wclayton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 18:19:29 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/03/31 21:28:02 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/04/07 10:30:57 by wclayton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,7 +111,7 @@ char				**ft_strsplit(char const *s, char c);
 char				*ft_itoa(int n);
 
 int					ft_atoi(const char *str);
-int     		    ft_atoi_base(char *nb, int base);
+int					ft_atoi_base(char *nb, int base);
 
 void				ft_putendl(char const *s);
 void				ft_putnbr(int n);
